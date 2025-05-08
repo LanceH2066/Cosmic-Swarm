7a6eec0f52a5451bcf2f6d3375a095e0cc3d048f
LanceH2066 <heinrichlance2066@gmail.com>
2025-05-07 18:42:02 -0700

    shield fix

--- src/_player.cpp
+++ src/_player.cpp
@@ -111,6 +111,7 @@ void _player::drawPlayer()
                 glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f, 0.0f);
                 glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f, 0.0f);
             glEnd();
+            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
             glPopMatrix();
         }
 
